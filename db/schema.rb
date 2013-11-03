08b9f4167943fa914f9675af2f9b914dd8956fbf
Viktor Palchyk <viktor.palchyk@gmail.com>
2013-11-03 23:49:19 +0200

    Where changes?

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131102104216) do
+ActiveRecord::Schema.define(version: 20131103210832) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -23,8 +23,9 @@ ActiveRecord::Schema.define(version: 20131102104216) do
     t.integer  "cart_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "quantity",   default: 1
+    t.integer  "quantity",                           default: 1
     t.integer  "order_id"
+    t.decimal  "price",      precision: 8, scale: 2
   end
 
   create_table "orders", force: true do |t|
